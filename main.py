1fa82c8b292e82948e976d8a69f6b09962b4b2f0
mmckenzie <m.g.g.mckenzie@student.tudelft.nl>
2021-02-23 21:37:08 +0100

    Making subfolder

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-# This is a sample Python script.
+from subfolder.funct import func1
+from test import f
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-from funct import func1
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print('Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 print(func1(2))
 # Press the green button in the gutter to run the script.
@@ -15,11 +13,12 @@ if __name__ == '__main__':
     print("yo")
     print('wsup')
 
-
 """
 Hi jorrit this is me
 beng beng beng
 Trying to screw things up 
 """
+
+print(f(5))
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
